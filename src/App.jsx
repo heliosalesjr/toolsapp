d00f7df7f1a258a759bdd07cb260b650b3495c50
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-04-06 21:10:02 -0300

    1st commit: shadcn, motion, tailwind and react icons installed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-pink-300 text-5xl text-center py-16 text-green-700 font-black">Hello bora lá</h1>
+    </>
+  )
+}
+
+export default App
